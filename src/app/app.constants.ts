c3dfe588270c3c29fa243304171ef5d03aa78c6a
rohith0101 <75571772+rohith0101@users.noreply.github.com>
2022-09-13 10:44:22 +0530

    Add files via upload

---
+++ src/app/app.constants.ts
@@ -0,0 +1,10 @@
+// export const API_URL = "http://authorization-lb-1831308262.ap-south-1.elb.amazonaws.com/"
+// export const CN_URL = "http://calculatenetworth-lb-435842482.ap-south-1.elb.amazonaws.com/"
+// export const MF_URL = "http://mutualfund-lb-1291275107.ap-south-1.elb.amazonaws.com/"
+// export const SD_URL = "http://shareprice-lb-1690499650.ap-south-1.elb.amazonaws.com/"
+
+
+export const API_URL = "http://localhost:8081"
+export const CN_URL = "http://localhost:8084"
+export const MF_URL = "http://localhost:8083"
+export const SD_URL = "http://localhost:8082"
